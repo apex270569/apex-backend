caa98bf3bb4ea5851aab721e73aeae611f881bda
apex270569 <apex270569@gmail.com>
2026-08-14 22:42:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
